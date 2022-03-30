97faa1d14d9582775d63ec65b6543c310091f95a
asen23 <andersenlim23@gmail.com>
2022-03-30 22:54:10 +0700

    add run to main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
+import Helper.Helper;
+import Menu.Auth.Auth;
+import Menu.Menu;
+
 public class Main {
     public static void main(String[] args) {
+        Helper.toggleDummy();
+        new Main().run();
+    }
+
+    public void run() {
+        Menu currentMenu;
+        do {
+            currentMenu = new Auth().run();
+            if(currentMenu != null) {
+                currentMenu.run();
+                continue;
+            }
+            break;
+        } while (true);
     }
 }
