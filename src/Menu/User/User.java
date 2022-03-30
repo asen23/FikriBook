21c37fd6a6f704c716670a85aa40b6b9369a31b6
asen23 <andersenlim23@gmail.com>
2022-03-30 22:46:23 +0700

    add user class

---
+++ src/Menu/User/User.java
@@ -0,0 +1,41 @@
+package Menu.User;
+
+public abstract class User {
+    private final String id;
+    private final String name;
+    private String email;
+    private String password;
+
+    public User(String id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public abstract UserType getUserType();
+}
