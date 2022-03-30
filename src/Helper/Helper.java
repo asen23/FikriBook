68a9cfa432f699580dabbffe0fa148dc66f3f353
asen23 <andersenlim23@gmail.com>
2022-03-30 22:48:23 +0700

    add feature to helper

--- src/Helper/Helper.java
+++ src/Helper/Helper.java
@@ -1,20 +1,90 @@
 package Helper;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class Helper {
     private static Scanner scan = new Scanner(System.in);
+    private static boolean isDummyEnabled = false;
+
+    public static void toggleDummy() {
+        isDummyEnabled = true;
+    }
+
+    public static boolean isDummyEnabled() {
+        return isDummyEnabled;
+    }
 
     public static void println(String s) {
         System.out.println(s);
     }
 
+    public static void println(String[] s) {
+        for (String string : s) {
+            System.out.println(string);
+        }
+    }
+
+    public static void printTable(String[] header, String headerSeparator, String[][] data, int[] pad, String separator) {
+        int totalLength = Arrays.stream(pad).sum() + header.length * separator.length() + 1;
+        headerSeparator = new String(new char[totalLength]).replace("\0", headerSeparator);
+        int[] fullPad = new int[header.length];
+        for (int i = 0; i < header.length; i++) {
+            if (i >= pad.length) {
+                fullPad[i] = 0;
+            } else {
+                fullPad[i] = pad[i];
+            }
+        }
+        Helper.println(headerSeparator);
+        for (int i = 0; i < header.length; i++) {
+            Helper.print(separator + pad(header[i], fullPad[i]));
+        }
+        Helper.println(separator);
+        Helper.println(headerSeparator);
+        for (String[] item : data) {
+            println(item, fullPad, separator);
+        }
+        Helper.println(headerSeparator);
+    }
+
+    public static void println(String[] s, int[] pad, String separator) {
+        int[] fullPad = new int[s.length];
+        for (int i = 0; i < s.length; i++) {
+            if (i >= pad.length) {
+                fullPad[i] = 0;
+            } else {
+                fullPad[i] = pad[i];
+            }
+        }
+        for (int i = 0; i < s.length; i++) {
+            Helper.print(separator + pad(s[i], fullPad[i]));
+        }
+        Helper.println(separator);
+    }
+
+    public static void println(String[] s, String separator) {
+        println(s, new int[0], separator);
+    }
+
     public static void println(String s, int pad) {
-        System.out.println(String.format("%-" + pad + "s", s));
+        System.out.println(pad(s, pad));
     }
 
     public static void println() {
         System.out.println();
+    }
+
+    public static void prompt() {
+        println();
+        getString(() -> {
+            print("Press enter to continue...");
+        });
+    }
+
+    public static String pad(String s, int pad) {
+        return String.format("%" + -pad + "s", s);
     }
 
     public static void print(String s) {
@@ -59,5 +129,9 @@ public class Helper {
             break;
         } while (true);
         return result;
+    }
+
+    public static String generateId(String prefix) {
+        return prefix + UUID.randomUUID().toString();
     }
 }
