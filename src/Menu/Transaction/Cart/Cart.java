7cf022f7e297625de88f40f473a1be38bc656ddd
asen23 <andersenlim23@gmail.com>
2022-04-05 16:12:22 +0700

    connect Cart class with Buyer change cartitem to use getter/setter

--- src/Menu/Transaction/Cart/Cart.java
+++ src/Menu/Transaction/Cart/Cart.java
@@ -2,6 +2,7 @@ package Menu.Transaction.Cart;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Cart {
     private final List<CartItem> items;
@@ -14,7 +15,18 @@ public class Cart {
         items.add(new CartItem(bookId, quantity));
     }
 
-    public List<CartItem> getItems() {
-        return items;
+    public Stream<CartItem> getItems() {
+        return items.stream();
+    }
+
+    public void editBookQuantity(String bookId, int quantity) {
+        if(quantity == 0) {
+            items.removeIf(cart -> cart.getBookId().equals(bookId));
+        }
+        items.forEach(cart -> {
+            if(cart.getBookId().equals(bookId)){
+                cart.setQuantity(quantity);
+            }
+        });
     }
 }
--- src/Menu/Transaction/Cart/CartItem.java
+++ src/Menu/Transaction/Cart/CartItem.java
@@ -1,11 +1,27 @@
 package Menu.Transaction.Cart;
 
 public class CartItem {
-    public String bookId;
-    public int quantity;
+    private String bookId;
+    private int quantity;
 
     public CartItem(String bookId, int quantity) {
         this.bookId = bookId;
+        this.quantity = quantity;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 }
