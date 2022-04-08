7e82b45800a3b478e6569070500d52b88a848ee9
MikazukiMizuki <winstonak13112001@gmail.com>
2022-04-08 15:25:11 +0700

    Edit admin menu dan menu

--- src/Menu/AdminMenu.java
+++ src/Menu/AdminMenu.java
@@ -1,212 +1,181 @@
 package Menu;
 
+import java.util.Collections;
+
 import Helper.Helper;
 import Menu.User.UserManager;
 
-import java.util.Collections;
-
 public class AdminMenu extends Menu {
-    protected static final UserManager userManager = UserManager.getInstance();
-    private final String[] adminMenu;
-    private final int currentMenuCount;
+	protected static final UserManager userManager = UserManager.getInstance();
+	private final String[] adminMenu;
+	private final int currentMenuCount;
 
-    public AdminMenu() {
-        this(true);
-    }
+	public AdminMenu() {
+		this(true);
+	}
 
-    protected AdminMenu(boolean initAdditionalMenu) {
-        super(false);
-        currentMenuCount = menuCount - 1;
-        adminMenu = new String[]{
-              menuCount++ +". Add Book",
-              menuCount++ +". Delete Book",
-              menuCount++ +". Edit Book",
-              menuCount++ +". List Transaction",
-              menuCount++ +". Display User",
-              menuCount++ +". Display User Detail",
-              menuCount++ +". Edit User",
-              menuCount++ +". Delete User"
-        };
-        if(initAdditionalMenu){
-            initAdditionalMenu();
-        }
-    }
+	protected AdminMenu(boolean initAdditionalMenu) {
+		super(false);
+		currentMenuCount = menuCount - 1;
+		adminMenu = new String[] { menuCount++ + ". Add Book", menuCount++ + ". Delete Book",
+				menuCount++ + ". Edit Book", menuCount++ + ". List Transaction", menuCount++ + ". Display User",
+				menuCount++ + ". Display User Detail", menuCount++ + ". Edit User", menuCount++ + ". Delete User" };
+		if (initAdditionalMenu) {
+			initAdditionalMenu();
+		}
+	}
 
-    @Override
-    protected String getHeaderSuffix() {
-        return "(Admin)";
-    }
+	@Override
+	protected String getHeaderSuffix() {
+		return "(Admin)";
+	}
 
-    @Override
-    protected String[] getMenu() {
-        return adminMenu;
-    }
+	@Override
+	protected String[] getMenu() {
+		return adminMenu;
+	}
 
-    @Override
-    protected boolean processMenu(int choice) {
-        switch (choice - currentMenuCount) {
-            case 1:
-                addBook();
-                break;
-            case 2:
-                deleteBook();
-                break;
-            case 3:
-                editBook();
-                break;
-            case 4:
-                listTransaction();
-                break;
-            case 5:
-            	listUser();
-            	Helper.prompt();
-            	break;
-            case 6:
-            	userDetail();
-            	break;
-            case 7:
-            	editUser();
-            	break;
-            case 8:
-            	deleteUser();
-            	break;
-        }
-        return super.processMenu(choice);
-    }
+	@Override
+	protected boolean processMenu(int choice) {
+		switch (choice - currentMenuCount) {
+		case 1:
+			addBook();
+			break;
+		case 2:
+			deleteBook();
+			break;
+		case 3:
+			editBook();
+			break;
+		case 4:
+			listTransaction();
+			break;
+		case 5:
+			listUser();
+			break;
+		case 6:
+			userDetail();
+			break;
+		case 7:
+			editUser();
+			break;
+		case 8:
+			deleteUser();
+			break;
+		}
+		return super.processMenu(choice);
+	}
 
-    protected void addBook() {
-        String title = Helper.getString(() -> Helper.print("Title :"));
-        String isbn = Helper.getString(() -> Helper.print("ISBN :"));
-        int page = Helper.getInt(() -> Helper.print("Page :"));
-        String author = Helper.getString(() -> Helper.print("Author :"));
-        String publisher = Helper.getString(() -> Helper.print("Publisher :"));
-        int price = Helper.getInt(() -> Helper.print("Price :"));
-        bookManager.addBook(
-              title,
-              isbn,
-              page,
-              Collections.singletonList(author),
-              publisher,
-              price
-        );
-        Helper.prompt("Added book successfully!");
-    }
+	protected void addBook() {
+		String title = Helper.getString(() -> Helper.print("Title :"));
+		String isbn = Helper.getString(() -> Helper.print("ISBN :"));
+		int page = Helper.getInt(() -> Helper.print("Page :"));
+		String author = Helper.getString(() -> Helper.print("Author :"));
+		String publisher = Helper.getString(() -> Helper.print("Publisher :"));
+		int price = Helper.getInt(() -> Helper.print("Price :"));
+		bookManager.addBook(title, isbn, page, Collections.singletonList(author), publisher, price);
+		Helper.prompt("Added book successfully!");
+	}
 
-    private String getBookId(String s) {
-        String bookId = Helper.getString(
-              () -> Helper.print("Book id to " + s + "(0 to cancel): ")
-        );
-        if (bookId.equals("0")) {
-            Helper.prompt("Canceled " + s + "!");
-            return null;
-        }
-        return bookId;
-    }
+	private String getBookId(String s) {
+		String bookId = Helper.getString(() -> Helper.print("Book id to " + s + "(0 to cancel): "));
+		if (bookId.equals("0")) {
+			Helper.prompt("Canceled " + s + "!");
+			return null;
+		}
+		return bookId;
+	}
 
-    protected void deleteBook() {
-        listBook();
-        String bookId = getBookId("delete");
-        try {
-            bookManager.deleteBook(bookId);
-            Helper.prompt("Successfully deleted book!");
-        } catch (Exception e) {
-            Helper.prompt(e.getMessage());
-        }
-    }
+	protected void deleteBook() {
+		listBook();
+		String bookId = getBookId("delete");
+		try {
+			bookManager.deleteBook(bookId);
+			Helper.prompt("Successfully deleted book!");
+		} catch (Exception e) {
+			Helper.prompt(e.getMessage());
+		}
+	}
 
-    protected void editBook() {
-        listBook();
-        String bookId = getBookId("edit");
-        int price = Helper.getInt(() -> Helper.print("Price : "));
-        try {
-            bookManager.editBook(bookId, price);
-            Helper.prompt("Successfully edited book!");
-        } catch (Exception e) {
-            Helper.prompt(e.getMessage());
-        }
-    }
+	protected void editBook() {
+		listBook();
+		String bookId = getBookId("edit");
+		int price = Helper.getInt(() -> Helper.print("Price : "));
+		try {
+			bookManager.editBook(bookId, price);
+			Helper.prompt("Successfully edited book!");
+		} catch (Exception e) {
+			Helper.prompt(e.getMessage());
+		}
+	}
 
-    protected void listTransaction() {
-        Helper.printTable(
-              new String[]{"ID", "Date", "Buyer"},
-              "=",
-              transactionManager.getTransaction()
-                    .map(transaction -> new String[]{
-                          transaction.getId(),
-                          transaction.getDateTime().toString(),
-                          userManager.getEmailWithId(transaction.getBuyerId()),
-                    })
-                    .toArray(String[][]::new),
-              new int[]{50, 20, 30},
-              "| "
-        );
-    }
-    
-    protected void listUser() {
-    	Helper.printTable(
-    			new String[] {"ID","Name","Email"},
-    			"=",
-    			userManager.listUser()
-    			.map(user -> new String[] {
-    					user.getId(),
-    					user.getName(),
-    					user.getEmail(),
-    			})
-    			.toArray(String[][]::new),
-    			new int[] {50,10,30},
-    			"| "
-    			);
-    	}
-    
-    protected void userDetail() {
-    	listUser();
-    	
-    	Helper.print("Input User ID: ");
-    	String userId = Helper.getString();
-    	
-    	Helper.println();
-    	
-    	Helper.println("Here is the Detail of your User!");
-    	Helper.println("User ID: "+userId);
-    	Helper.println("Name: "+userManager.getName(userId));
-    	Helper.println("Email: "+userManager.getEmail(userId));
-    	Helper.println("Password: "+userManager.getPassword(userId));
-    	Helper.prompt();
-    }
-    
-    private String getUserId(String s) {
-        String userId = Helper.getString(
-              () -> Helper.print("User id to " + s + "(0 to cancel): ")
-        );
-        if (userId.equals("0")) {
-            Helper.prompt("Canceled " + s + "!");
-            return null;
-        }
-        return userId;
-    }
-    
-    protected void deleteUser() {
-        listUser();
-        String userId = getUserId("delete");
-        try {
-            userManager.deleteUser(userId);
-            Helper.prompt("Successfully deleted user!");
-        } catch (Exception e) {
-            Helper.prompt(e.getMessage());
-        }
-    }
-    
-    protected void editUser() {
-    	listUser();
-        String userId = getUserId("edit");
-        String email = Helper.getString(() -> Helper.print("Email : "));;
-        String password = Helper.getString(() -> Helper.print("Password : "));;
-        try {
-        	userManager.editUser(userId, email, password);
-            Helper.prompt("Successfully edited user!");
-        } catch (Exception e) {
-            Helper.prompt(e.getMessage());
-        }
-    }
-    
+	protected void listTransaction() {
+		Helper.printHeader("Transaction List");
+		Helper.printTable(new String[] { "ID", "Date", "Buyer" }, "=",
+				transactionManager.getTransaction()
+						.map(transaction -> new String[] { transaction.getId(), transaction.getDateTime().toString(),
+								userManager.getEmailWithId(transaction.getBuyerId()), })
+						.toArray(String[][]::new),
+				new int[] { 50, 20, 30 }, "| ");
+	}
+
+	protected void listUser() {
+		Helper.printHeader("User List");
+		Helper.printTable(new String[] { "ID", "Name", "Email" }, "=", userManager.listUser()
+				.map(user -> new String[] { user.getId(), user.getName(), user.getEmail(), }).toArray(String[][]::new),
+				new int[] { 50, 10, 30 }, "| ");
+		Helper.prompt();
+	}
+
+	protected void userDetail() {
+		listUser();
+
+		Helper.print("Input User ID: ");
+		String userId = Helper.getString();
+
+		Helper.println();
+
+		Helper.println("Here is the Detail of your User!");
+		Helper.println("User ID: " + userId);
+		Helper.println("Name: " + userManager.getName(userId));
+		Helper.println("Email: " + userManager.getEmail(userId));
+		Helper.println("Password: " + userManager.getPassword(userId));
+		Helper.prompt();
+	}
+
+	private String getUserId(String s) {
+		String userId = Helper.getString(() -> Helper.print("User id to " + s + "(0 to cancel): "));
+		if (userId.equals("0")) {
+			Helper.prompt("Canceled " + s + "!");
+			return null;
+		}
+		return userId;
+	}
+
+	protected void deleteUser() {
+		listUser();
+		String userId = getUserId("delete");
+		try {
+			userManager.deleteUser(userId);
+			Helper.prompt("Successfully deleted user!");
+		} catch (Exception e) {
+			Helper.prompt(e.getMessage());
+		}
+	}
+
+	protected void editUser() {
+		listUser();
+		String userId = getUserId("edit");
+		String email = Helper.getString(() -> Helper.print("Email : "));
+		;
+		String password = Helper.getString(() -> Helper.print("Password : "));
+		;
+		try {
+			userManager.editUser(userId, email, password);
+			Helper.prompt("Successfully edited user!");
+		} catch (Exception e) {
+			Helper.prompt(e.getMessage());
+		}
+	}
+
 }
--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -3,145 +3,135 @@ package Menu;
 import Helper.Helper;
 import Menu.Book.BookManager;
 import Menu.Transaction.TransactionManager;
+import Menu.User.User;
 import Menu.User.UserManager;
 
 public abstract class Menu {
 	protected static final UserManager userManager = UserManager.getInstance();
-    protected static BookManager bookManager = BookManager.getInstance();
-    protected static TransactionManager transactionManager = TransactionManager.getInstance();
-    protected String[] menu;
-    protected int menuCount = 1;
+	protected static BookManager bookManager = BookManager.getInstance();
+	protected static TransactionManager transactionManager = TransactionManager.getInstance();
+	protected String[] menu;
+	protected int menuCount = 1;
 
-    public Menu() {
-        this(true);
-    }
+	public Menu() {
+		this(true);
+	}
 
-    protected Menu(boolean initAdditionalMenu) {
-        menu = new String[]{
-              menuCount++ + ". Display book",
-              menuCount++ + ". Display book detail",
-              menuCount++ +". View My Profile Data",
-              menuCount++ +". Edit My Profile Data",
-        };
-        if (initAdditionalMenu) {
-            initAdditionalMenu();
-        }
-    }
+	protected Menu(boolean initAdditionalMenu) {
+		menu = new String[] { menuCount++ + ". Display book", menuCount++ + ". Display book detail",
+				menuCount++ + ". View My Profile Data", menuCount++ + ". Edit My Profile Data", };
+		if (initAdditionalMenu) {
+			initAdditionalMenu();
+		}
+	}
 
-    protected void initAdditionalMenu() {
-        menu = Helper.concatArray(menu, getMenu());
-        menu = Helper.concatArray(
-              menu,
-              new String[]{
-                    "0. Logout",
-              }
-        );
-    }
+	protected void initAdditionalMenu() {
+		menu = Helper.concatArray(menu, getMenu());
+		menu = Helper.concatArray(menu, new String[] { "0. Logout", });
+	}
 
-    protected abstract String getHeaderSuffix();
+	protected abstract String getHeaderSuffix();
 
-    public void run() {
-        while (true) {
-            Helper.cls();
-            Helper.printHeader("FikriBook Menu " + getHeaderSuffix());
-            Helper.println(menu);
-            int choice = Helper.getInt(() -> Helper.print(">> "));
-            boolean exit = processMenu(choice);
-            if (exit) {
-                return;
-            }
-        }
-    }
+	public void run() {
+		while (true) {
+			Helper.cls();
+			Helper.printHeader("FikriBook Menu " + getHeaderSuffix());
+			Helper.println(menu);
+			int choice = Helper.getInt(() -> Helper.print(">> "));
+			boolean exit = processMenu(choice);
+			if (exit) {
+				return;
+			}
+		}
+	}
 
-    protected abstract String[] getMenu();
+	protected abstract String[] getMenu();
 
-    protected boolean processMenu(int choice) {
-        switch (choice) {
-            case 1:
-                listBook();
-                Helper.prompt();
-                break;
-            case 2:
-            	bookDetail();
-            	break;
-            case 3:
-            	myUserDetail();
-            	break;
-            case 4:
-            	myEditUser();
-            	break;
-            case 0:
-                return true;
-        }
-        return false;
-    }
+	protected boolean processMenu(int choice) {
+		switch (choice) {
+		case 1:
+			listBook();
+			break;
+		case 2:
+			bookDetail();
+			break;
+		case 3:
+			myUserDetail();
+			break;
+		case 4:
+			myEditUser();
+			break;
+		case 0:
+			return true;
+		}
+		return false;
+	}
 
-    protected void listBook() {
-        Helper.cls();
-        Helper.printHeader("Book List");
-        printBookList();
-        Helper.prompt();
-    }
+	protected void listBook() {
+		Helper.cls();
+		Helper.printHeader("Book List");
+		printBookList();
+		Helper.prompt();
+	}
 
-    protected void printBookList(){
-        Helper.printTable(
-                new String[]{"ID", "Title", "ISBN", "Page", "Authors", "Publisher", "Price"},
-                "=",
-                bookManager.listBook()
-                        .map((book) -> new String[]{
-                                book.getId(),
-                                book.getTitle(),
-                                book.getIsbn(),
-                                Integer.toString(book.getPage()),
-                                book.getAuthors().get(0),
-                                book.getPublisher(),
-                                Integer.toString(book.getPrice()),
-                        }).toArray(String[][]::new),
-                new int[]{50, 10, 10, 6, 15, 15, 10},
-                "| "
-        );
-    }
-    
-    protected void bookDetail() {
-    	Helper.print("Input Book ID: ");
-    	String bookId = Helper.getString();
-    	
-    	Helper.println();
-    	
-    	Helper.println("Here is the Detail of your book!");
-    	Helper.println("Book ID: "+bookId);
-    	Helper.println("Title: "+bookManager.getTitle(bookId));
-    	Helper.println("ISBN: "+bookManager.getIsbn(bookId));
-    	Helper.println("Page: "+bookManager.getPage(bookId));
-    	Helper.println("Authors: "+bookManager.getAuthors(bookId));
-    	Helper.println("Publisher: "+bookManager.getPublisher(bookId));
-    	Helper.println("Price: "+bookManager.getPrice(bookId));
-    	
-    	Helper.prompt();
-    }
-    
-    protected void myUserDetail() {
-    	String userId="";
-    	//get dengan cara apapun itu
-    	
-    	Helper.println("Here is the Detail of your Profile!");
-    	Helper.println("User ID: "+userId);
-    	Helper.println("Name: "+userManager.getName(userId));
-    	Helper.println("Email: "+userManager.getEmail(userId));
-    }
-    
-    
-    protected void myEditUser() {
-    	myUserDetail();
-    	String userId="";
-//        String userId = getUserId("edit");
-        String email = Helper.getString(() -> Helper.print("Email : "));;
-        String password = Helper.getString(() -> Helper.print("Password : "));;
-        try {
-        	userManager.editUser(userId, email, password);
-            Helper.prompt("Successfully edited book!");
-        } catch (Exception e) {
-            Helper.prompt(e.getMessage());
-        }
-    }
+	protected void printBookList() {
+		Helper.printTable(new String[] { "ID", "Title", "ISBN", "Page", "Authors", "Publisher", "Price" }, "=",
+				bookManager.listBook()
+						.map((book) -> new String[] { book.getId(), book.getTitle(), book.getIsbn(),
+								Integer.toString(book.getPage()), book.getAuthors().get(0), book.getPublisher(),
+								Integer.toString(book.getPrice()), })
+						.toArray(String[][]::new),
+				new int[] { 50, 10, 10, 6, 15, 15, 10 }, "| ");
+	}
+
+	protected void bookDetail() {
+		Helper.print("Input Book ID: ");
+		String bookId = Helper.getString();
+
+		Helper.println();
+
+		Helper.printHeader("Here is the Detail of your book!");
+		Helper.println("Book ID: " + bookId);
+		Helper.println("Title: " + bookManager.getTitle(bookId));
+		Helper.println("ISBN: " + bookManager.getIsbn(bookId));
+		Helper.println("Page: " + bookManager.getPage(bookId));
+		Helper.println("Authors: " + bookManager.getAuthors(bookId));
+		Helper.println("Publisher: " + bookManager.getPublisher(bookId));
+		Helper.println("Price: " + bookManager.getPrice(bookId));
+
+		Helper.prompt();
+	}
+
+	protected String getMyUserDetail() {
+		User userTemp = userManager.getCurrentUser();
+		String userId = userTemp.getId();
+		return userId;
+	}
+
+	protected void myUserDetail() {
+		String userId = getMyUserDetail();
+		Helper.println("Here is the Detail of your Profile!");
+		Helper.println("User ID: " + userId);
+		Helper.println("Name: " + userManager.getName(userId));
+		Helper.println("Email: " + userManager.getEmail(userId));
+
+		Helper.prompt();
+	}
+
+	protected void myEditUser() {
+		myUserDetail();
+
+		String userId = getMyUserDetail();
+
+		String email = Helper.getString(() -> Helper.print("Email : "));
+		;
+		String password = Helper.getString(() -> Helper.print("Password : "));
+		;
+		try {
+			userManager.editUser(userId, email, password);
+			Helper.prompt("Successfully edited book!");
+		} catch (Exception e) {
+			Helper.prompt(e.getMessage());
+		}
+	}
 }
