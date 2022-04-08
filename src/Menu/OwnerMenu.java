342bf0d33d14768bd7ae99bdb8972fc0ef0527a4
asen23 <andersenlim23@gmail.com>
2022-04-08 20:29:04 +0700

    implement owner menu add id check add email check

--- src/Menu/OwnerMenu.java
+++ src/Menu/OwnerMenu.java
@@ -1,6 +1,9 @@
 package Menu;
 
 import Helper.Helper;
+import Menu.User.Admin;
+
+import java.time.LocalDate;
 
 public class OwnerMenu extends AdminMenu {
     private final String[] ownerMenu;
@@ -40,15 +43,111 @@ public class OwnerMenu extends AdminMenu {
     protected boolean processMenu(int choice) {
         switch (choice - currentMenuCount) {
             case 1:
-                Helper.println("1");
+                addAdmin();
                 break;
             case 2:
-                Helper.println("2");
+                deleteAdmin();
                 break;
             case 3:
-                Helper.println("3");
+                toggleAdminStatus();
                 break;
         }
         return super.processMenu(choice);
+    }
+
+    private void addAdmin() {
+        while (true) {
+            Helper.cls();
+            Helper.printHeader("Add Admin");
+            String name = Helper.getString(() -> Helper.print("Name : "));
+            if (name.isEmpty()) return;
+            String email = Helper.getString(
+                  () -> Helper.print("Email : "),
+                  input -> {
+                      if (userManager.isValidEmail(input)) return false;
+                      Helper.println("Invalid email format!");
+                      return true;
+                  },
+                  String::isEmpty
+            );
+            if (email.isEmpty()) return;
+            String password = Helper.getString(
+                  () -> Helper.print("Password : "),
+                  input -> {
+                      String errorMessage = userManager.isValidPassword(input);
+                      if (errorMessage.isEmpty()) return false;
+                      Helper.println(errorMessage);
+                      return true;
+                  },
+                  String::isEmpty
+            );
+            if (password.isEmpty()) return;
+            try {
+                userManager.registerAdmin(
+                      name,
+                      email,
+                      password
+                );
+            } catch (Exception e) {
+                Helper.println(e.getMessage());
+                continue;
+            }
+            break;
+        }
+        Helper.prompt("Successfully added admin!");
+    }
+
+    private void printAdminList() {
+        Helper.printTable(
+              new String[]{"ID", "Name", "Email", "Active"},
+              "=",
+              userManager.listAdmin()
+                    .map(admin -> new String[]{
+                          admin.getId(),
+                          admin.getName(),
+                          admin.getEmail(),
+                          String.valueOf(admin.isActive())
+                    })
+                    .toArray(String[][]::new),
+              new int[]{50, 15, 30, 10},
+              "| "
+        );
+    }
+
+    private String getAdminId() {
+        printAdminList();
+
+        return Helper.getString(
+              () -> Helper.print("Input Admin ID : "),
+              id -> {
+                  if(userManager.checkIdExist(id)) return false;
+                  Helper.println("Invalid Admin ID");
+                  return true;
+              },
+              String::isEmpty
+        );
+    }
+
+    private void deleteAdmin() {
+        Helper.cls();
+        Helper.printHeader("Delete Admin");
+
+        String adminId = getAdminId();
+        if (adminId.isEmpty()) return;
+
+        userManager.deleteAdmin(adminId);
+        Helper.prompt("Deleted admin successfully");
+    }
+
+    private void toggleAdminStatus() {
+        Helper.cls();
+        Helper.printHeader("Toggle Admin Active Status");
+
+        String adminId = getAdminId();
+        if (adminId.isEmpty()) return;
+
+        Admin admin = (Admin)userManager.getUser(adminId);
+        admin.setActive(!admin.isActive());
+        Helper.prompt("Toggled admin active status successfully");
     }
 }
--- src/Menu/User/UserManager.java
+++ src/Menu/User/UserManager.java
@@ -18,61 +18,61 @@ public class UserManager {
         users.add(new Owner(generateId(), "Owner", "owner@fikribook.com", "idk"));
         if (Helper.isDummyEnabled()) {
             users.add(new Admin(
-                    generateId(),
-                    "Admin1",
-                    "admin1@fikribook.com",
-                    "Admin1",
-                    true
+                  generateId(),
+                  "Admin1",
+                  "admin1@fikribook.com",
+                  "Admin1",
+                  true
             ));
             users.add(new Admin(
-                    generateId(),
-                    "Admin2",
-                    "admin2@fikribook.com",
-                    "Admin2",
-                    false
+                  generateId(),
+                  "Admin2",
+                  "admin2@fikribook.com",
+                  "Admin2",
+                  false
             ));
             users.add(new Admin(
-                    generateId(),
-                    "Admin3",
-                    "admin3@fikribook.com",
-                    "Admin3",
-                    true
+                  generateId(),
+                  "Admin3",
+                  "admin3@fikribook.com",
+                  "Admin3",
+                  true
             ));
             users.add(new Buyer(
-                    "user-fba9b13e-c438-48cc-ad65-670c3d356e40",
-                    "buyer1",
-                    "buyer1@buyer.com",
-                    "buyer1",
-                    LocalDate.of(2000, 1, 1),
-                    "buyer1",
-                    "buyer1"
+                  "user-fba9b13e-c438-48cc-ad65-670c3d356e40",
+                  "buyer1",
+                  "buyer1@buyer.com",
+                  "buyer1",
+                  LocalDate.of(2000, 1, 1),
+                  "buyer1",
+                  "buyer1"
             ));
             users.add(new Buyer(
-                    "user-adb4ff58-8d7a-45e6-a733-540ea4aa2545",
-                    "buyer2",
-                    "buyer2@buyer.com",
-                    "buyer2",
-                    LocalDate.of(2000, 2, 2),
-                    "buyer2",
-                    "buyer2"
+                  "user-adb4ff58-8d7a-45e6-a733-540ea4aa2545",
+                  "buyer2",
+                  "buyer2@buyer.com",
+                  "buyer2",
+                  LocalDate.of(2000, 2, 2),
+                  "buyer2",
+                  "buyer2"
             ));
             users.add(new Buyer(
-                    "user-9970db0b-048f-4321-9cd8-b7717818e456",
-                    "buyer3",
-                    "buyer3@buyer.com",
-                    "buyer3",
-                    LocalDate.of(2000, 3, 3),
-                    "buyer3",
-                    "buyer3"
+                  "user-9970db0b-048f-4321-9cd8-b7717818e456",
+                  "buyer3",
+                  "buyer3@buyer.com",
+                  "buyer3",
+                  LocalDate.of(2000, 3, 3),
+                  "buyer3",
+                  "buyer3"
             ));
             users.add(new Buyer(
-                    "user-26963a6d-1c91-4f09-92dc-865249a80d0f",
-                    "buyer4",
-                    "buyer4@buyer.com",
-                    "buyer4",
-                    LocalDate.of(2000, 4, 4),
-                    "buyer4",
-                    "buyer4"
+                  "user-26963a6d-1c91-4f09-92dc-865249a80d0f",
+                  "buyer4",
+                  "buyer4@buyer.com",
+                  "buyer4",
+                  LocalDate.of(2000, 4, 4),
+                  "buyer4",
+                  "buyer4"
             ));
         }
     }
@@ -87,16 +87,16 @@ public class UserManager {
 
     private Optional<User> getUserWithEmail(String email) {
         return users
-                .stream()
-                .filter((u) -> u.getEmail().equals(email))
-                .findFirst();
+              .stream()
+              .filter((u) -> u.getEmail().equals(email))
+              .findFirst();
     }
 
     public String getEmailWithId(String id) {
         Optional<User> user = users
-                .stream()
-                .filter((u) -> u.getId().equals(id))
-                .findFirst();
+              .stream()
+              .filter((u) -> u.getId().equals(id))
+              .findFirst();
         return user.map(User::getEmail).orElse("");
     }
 
@@ -108,59 +108,96 @@ public class UserManager {
         return currentUser;
     }
 
-    public void register(
-            String name,
-            String email,
-            String password,
-            LocalDate dob,
-            String address,
-            String phoneNumber
-    ) throws Exception {
+    public boolean checkIdExist(String id) {
+        return users
+              .stream()
+              .anyMatch(user -> user.getId().equals(id));
+    }
+
+    private void checkEmailExist(String email) throws Exception {
         Optional<User> user = getUserWithEmail(email);
         if (user.isPresent()) throw new Exception("Email already exist!");
+    }
+
+    public void register(
+          String name,
+          String email,
+          String password,
+          LocalDate dob,
+          String address,
+          String phoneNumber
+    ) throws Exception {
+        checkEmailExist(email);
         users.add(new Buyer(
-                generateId(),
-                name,
-                email,
-                password,
-                dob,
-                address,
-                phoneNumber
+              generateId(),
+              name,
+              email,
+              password,
+              dob,
+              address,
+              phoneNumber
         ));
+    }
+
+    public void registerAdmin(
+          String name,
+          String email,
+          String password
+    ) throws Exception {
+        checkEmailExist(email);
+        users.add(new Admin(
+              generateId(),
+              name,
+              email,
+              password,
+              true
+        ));
+    }
+
+    public void deleteAdmin(String id) {
+        users.removeIf(user -> user.getId().equals(id));
+    }
+
+    public User getUser(String id) {
+        return users
+              .stream()
+              .filter(user -> user.getId().equals(id))
+              .findFirst()
+              .orElse(null);
     }
 
     public String getName(String userId) {
         return users
-                .stream()
-                .filter((user) -> user.getId().equals(userId))
-                .findFirst()
-                .map(User::getName)
-                .orElse("404 Not Found");
+              .stream()
+              .filter((user) -> user.getId().equals(userId))
+              .findFirst()
+              .map(User::getName)
+              .orElse("404 Not Found");
     }
 
     public String getEmail(String userId) {
         return users
-                .stream()
-                .filter((user) -> user.getId().equals(userId))
-                .findFirst()
-                .map(User::getEmail)
-                .orElse("404 Not Found");
+              .stream()
+              .filter((user) -> user.getId().equals(userId))
+              .findFirst()
+              .map(User::getEmail)
+              .orElse("404 Not Found");
     }
 
     public String getPassword(String userId) {
         return users
-                .stream()
-                .filter((user) -> user.getId().equals(userId))
-                .findFirst()
-                .map(User::getPassword)
-                .orElse("404 Not Found");
+              .stream()
+              .filter((user) -> user.getId().equals(userId))
+              .findFirst()
+              .map(User::getPassword)
+              .orElse("404 Not Found");
     }
 
     public void deleteUser(String id) throws Exception {
         Optional<User> user = users
-                .stream()
-                .filter((b) -> b.getId().equals(id))
-                .findFirst();
+              .stream()
+              .filter((b) -> b.getId().equals(id))
+              .findFirst();
         if (!user.isPresent()) {
             throw new Exception("Invalid user id!");
         }
@@ -169,9 +206,9 @@ public class UserManager {
 
     public void editUser(String id, String email, String password) throws Exception {
         Optional<User> user = users
-                .stream()
-                .filter((b) -> b.getId().equals(id))
-                .findFirst();
+              .stream()
+              .filter((b) -> b.getId().equals(id))
+              .findFirst();
         if (!user.isPresent()) {
             throw new Exception("Invalid user id!");
         }
@@ -183,11 +220,18 @@ public class UserManager {
         return users.stream();
     }
 
+    public Stream<Admin> listAdmin() {
+        return users
+              .stream()
+              .filter(user -> user instanceof Admin)
+              .map(user -> (Admin)user);
+    }
+
     private Buyer getBuyer(String userId) throws Exception {
         Optional<User> user = users
-                .stream()
-                .filter((b) -> b.getId().equals(userId))
-                .findFirst();
+              .stream()
+              .filter((b) -> b.getId().equals(userId))
+              .findFirst();
         if (!user.isPresent()) {
             throw new Exception("Invalid user id!");
         }
@@ -203,23 +247,22 @@ public class UserManager {
         return buyer.getCartItem();
     }
 
-
     public void addCartItem(
-            String userId,
-            String bookId,
-            int quantity
+          String userId,
+          String bookId,
+          int quantity
     ) throws Exception {
         Buyer buyer = getBuyer(userId);
-        buyer.addCartItem(bookId,quantity);
+        buyer.addCartItem(bookId, quantity);
     }
 
     public void editCartItem(
-            String userId,
-            String bookId,
-            int quantity
+          String userId,
+          String bookId,
+          int quantity
     ) throws Exception {
         Buyer buyer = getBuyer(userId);
-        buyer.editCartItem(bookId,quantity);
+        buyer.editCartItem(bookId, quantity);
     }
 
     public User getCurrentUser() {
@@ -235,16 +278,16 @@ public class UserManager {
     }
 
     public String isValidPassword(String password) {
-        if(password.length() < 8) {
+        if (password.length() < 8) {
             return "Password should be at least 8 characters long";
         }
-        if(!password.matches(".*[A-Z].*")) {
+        if (!password.matches(".*[A-Z].*")) {
             return "Password should include at least one uppercase character";
         }
-        if(!password.matches(".*[a-z].*")) {
+        if (!password.matches(".*[a-z].*")) {
             return "Password should include at least one lowercase character";
         }
-        if(!password.matches(".*[0-9].*")) {
+        if (!password.matches(".*[0-9].*")) {
             return "Password should include at least one number";
         }
         return "";
